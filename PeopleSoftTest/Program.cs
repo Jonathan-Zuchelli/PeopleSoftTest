a59b5b6c9c189ceb28e177de73e0e29fcb96f65c
Jonathan Zuchelli <jrzuchelli@gmail.com>
2020-11-17 09:13:12 -0500

    Initial Commit

---
+++ PeopleSoftTest/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace PeopleSoftTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string result = await TestPeopleSoftEndpoint();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        private static async Task<string> TestPeopleSoftEndpoint()
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            HttpClient client = new HttpClient();
+
+            TokenModel authenticatedToken = null;
+
+            var values = new Dictionary<string, string>
+            {
+                {"grant_type", "cps" },
+                {"client_id","v3app" },
+                {"client_secret", "v4secret" },
+                {"scope", "openid profile offline_access thirdparty" },
+                {"apikey", "5KDgPVINVd7ye4f4VLAzNofcm5ZRpLyEOYYkP7uEuMulW98GS5MMHQ==" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            try
+            {
+                Uri baseUri = new Uri("https://cityofcalgarytest.cps.golf/");
+                Uri v4AuthUri = new Uri(baseUri, "identityapi/connect/token");
+                var response = client.PostAsync(v4AuthUri.ToString(), content).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+
+                if (!string.IsNullOrEmpty(responseString))
+                {
+                    authenticatedToken = JsonConvert.DeserializeObject<TokenModel>(responseString);
+                }
+
+                if(authenticatedToken != null)
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticatedToken.access_token);
+
+                    string queryString = "fromDate=2020-10-1&toDate=2020-11-11";
+
+                    Uri apiUrl = new Uri(baseUri, $"thirdpartyapi/api/v1/PeopleSoft/GlData?{queryString}");
+
+                    var contentReturn = await client.GetStringAsync(apiUrl);
+
+                    return contentReturn;
+                }
+
+            }
+            catch
+            {
+                
+            }
+
+            return "Error";
+        }
+
+        private class TokenModel
+        {
+            public string access_token { get; set; }
+            public int expires_in { get; set; }
+            public string token_type { get; set; }
+            public string refresh_token { get; set; }
+            public string scope { get; set; }
+        }
+    }
+}
